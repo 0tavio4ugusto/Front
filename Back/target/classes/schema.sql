e8e018f7acbcde17665c00d8caf86a20db3f66aa
0tavio4ugusto <otavioaugustor17@gmail.com>
2026-09-07 18:34:02 +0000

    fix: remove target/ compiled classes from repo

--- Back/target/classes/schema.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS tarefa (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    titulo VARCHAR(100) NOT NULL,
-    descricao VARCHAR(255),
-    prioridade VARCHAR(10),
-    categoria VARCHAR(20),
-    data VARCHAR(10)
-    );
